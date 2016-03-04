3e24977d4303b0b830897aab23e1cea19bafc9f6
dawdlerstop <1138568289@qq.com>
2016-03-04 11:45:05 +0800

    first

---
+++ MyFirstItem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstItem
+//
+//  Created by 王维 on 16/3/4.
+//  Copyright © 2016年 王维. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
